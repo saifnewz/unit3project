415700e742e8985fbc78d752bf4a64e89582e8e9
saifnewz <saif.newaz@gmail.com>
2022-07-16 15:00:25 -0700

    added styling to cards

--- my-app/src/components/Quote.jsx
+++ my-app/src/components/Quote.jsx
@@ -4,9 +4,9 @@ const Quote = (props) => {
    
     return (
         
-        <div>
+        <div className="column">
             {props.quotes.map((quote) => (
-                <div key={quote.anime}>
+                <div key={quote.anime} className="text">
                     <h2>{quote.anime}</h2>
                     <h3>{quote.character}</h3>
                     <p>"{quote.quote}"</p>
